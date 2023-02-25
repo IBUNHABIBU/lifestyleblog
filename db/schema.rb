cd61d0e9a83e1d59f54e7e5dd174c5656673c2f1
Ibunhabibu <Ibunhabib@gmail.com>
2023-02-25 14:13:07 +0300

    Add article model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_10_172523) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_25_111017) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.string "likes_count"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,4 +32,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_10_172523) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "articles", "users"
 end
