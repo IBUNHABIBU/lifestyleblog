a0a20fb103a24dc01a4746b1c6509bcf6844c82f
Ibunhabibu <Ibunhabib@gmail.com>
2023-02-26 14:44:55 +0300

    Add likes associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false },
                     format: /\A\S+@\S+\z/
   has_many :articles, dependent: :destroy
-
+  has_many :likes, dependent: :destroy
+  has_many :liked_articles, through: :likes, source: :article
+  
     def self.authenticate(email, password)
         user = User.find_by(email: email)
         user && user.authenticate(password)
