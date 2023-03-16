1ba6cfe9ed6ee3b333a83823115911274bf0105c
Ibunhabibu <Ibunhabib@gmail.com>
2023-03-16 19:58:11 +0300

    Add most liked image

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,7 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
     @recent_articles = Article.recent
-    @most_rated_articles = Article.first
+    @most_rated_articles = Article.most_rated.first
   end
 
   # GET /articles/1
